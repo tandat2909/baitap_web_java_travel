bd30f18c50a962ee8f5f1a0b976ec80c97dba87d
Tan Dat <1851050032dat@ou.edu.vn>
2021-05-10 23:44:43 +0700

    import MethodSorters

--- Project/src/test/java/com/travels/springmvc/services/implement/AccountServiceTest.java
+++ Project/src/test/java/com/travels/springmvc/services/implement/AccountServiceTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
